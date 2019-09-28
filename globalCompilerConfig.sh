2aaa6d7af02b893e3b3f885f31c33c24f83946f7
olivier st-laurent <kzaurckichz@gmail.com>
2019-09-28 04:23:28 -0400

    ..

--- globalCompilerConfig.sh
+++ globalCompilerConfig.sh
@@ -2,10 +2,8 @@
 # Warning Flags
 GCC_FLAGS="$GCC_FLAGS -Wall"
 GCC_FLAGS="$GCC_FLAGS -Wextra"
-GCC_FLAGS="$GCC_FLAGS -Werror"
 GCC_FLAGS="$GCC_FLAGS -Wnon-virtual-dtor"
 GCC_FLAGS="$GCC_FLAGS -Wcast-align"
-GCC_FLAGS="$GCC_FLAGS -Wunused"
 GCC_FLAGS="$GCC_FLAGS -Woverloaded-virtual"
 GCC_FLAGS="$GCC_FLAGS -Wconversion"
 GCC_FLAGS="$GCC_FLAGS -Wsign-conversion"
@@ -15,5 +13,10 @@ GCC_FLAGS="$GCC_FLAGS -Wformat=2"
 GCC_FLAGS="$GCC_FLAGS -Wduplicated-cond"
 GCC_FLAGS="$GCC_FLAGS -Wduplicated-branches"
 GCC_FLAGS="$GCC_FLAGS -Wlogical-op"
+GCC_FLAGS="$GCC_FLAGS -Wunused"
+GCC_FLAGS="$GCC_FLAGS -Wpessimizing-move"
+GCC_FLAGS="$GCC_FLAGS -Wredundant-move"
+
+# GCC_FLAGS="$GCC_FLAGS -Werror"
 # GCC_FLAGS="$GCC_FLAGS -Wshadow"
 
