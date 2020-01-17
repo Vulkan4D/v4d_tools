9278c901954808bdd78e62c20b10dace4366374c
olivier st-laurent <kzaurckichz@gmail.com>
2020-01-16 23:52:41 -0500

    Refactoring Modules, WIP incubator galaxy4d module

--- assetcompiler/models.cmake
+++ assetcompiler/models.cmake
@@ -1,14 +1,17 @@
-file(GLOB_RECURSE srcModels RELATIVE ${ASSETS_RELATIVE_SRC}/ ${ASSETS_GLOB_SEARCH}/models/**)
-foreach(model ${srcModels})
-	set(fileInput ${ASSETS_RELATIVE_SRC}/${model})
-	set(fileOutput ${RUNTIME_OUTPUT_DIRECTORY}/${model})
-	add_custom_command(
-		MAIN_DEPENDENCY ${fileInput}
-		OUTPUT ${fileOutput} POST_BUILD
-		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-		COMMAND ${CMAKE_COMMAND} -E copy ${fileInput} ${fileOutput}
-	)
-	list(APPEND copiedModels ${fileOutput})
+foreach(dir ${ASSETS_DIRS})
+	file(GLOB_RECURSE srcModels RELATIVE ${ASSETS_RELATIVE_SRC}/ ${dir}/models/**)
+	foreach(model ${srcModels})
+		string(REGEX REPLACE "^v4d/" "" modelPath ${model})
+		set(fileInput ${ASSETS_RELATIVE_SRC}/${model})
+		set(fileOutput ${RUNTIME_OUTPUT_DIRECTORY}/${modelPath})
+		add_custom_command(
+			MAIN_DEPENDENCY ${fileInput}
+			OUTPUT ${fileOutput} POST_BUILD
+			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+			COMMAND ${CMAKE_COMMAND} -E copy ${fileInput} ${fileOutput}
+		)
+		list(APPEND copiedModels ${fileOutput})
+	endforeach()
 endforeach()
 
 add_custom_target(models ALL DEPENDS ${copiedModels})
--- assetcompiler/shaders.cmake
+++ assetcompiler/shaders.cmake
@@ -10,21 +10,24 @@ set_target_properties(shadercompiler
 		RUNTIME_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}"
 )
 
-file(GLOB_RECURSE srcShaders RELATIVE ${ASSETS_RELATIVE_SRC}/ ${ASSETS_GLOB_SEARCH}/shaders/**)
-foreach(shader ${srcShaders})
-	string(REGEX MATCH ".*/[a-zA-Z0-9][^/]*\.(conf|glsl|hlsl|vert|tesc|tese|geom|frag|comp|mesh|task|rgen|rint|rahit|rchit|rmiss|rcall)$" IsShader ${shader})
-	if(IsShader)
-		string(REGEX REPLACE "\.glsl$" "" shaderFileName ${shader})
-		set(shaderInput ${ASSETS_RELATIVE_SRC}/${shader})
-		set(shaderOutput ${RUNTIME_OUTPUT_DIRECTORY}/${shaderFileName}.meta)
-		add_custom_command(
-			MAIN_DEPENDENCY ${shaderInput}
-			OUTPUT ${shaderOutput} POST_BUILD
-			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-			COMMAND ${CMAKE_BINARY_DIR}/shadercompiler${CMAKE_EXECUTABLE_SUFFIX} ${shaderInput} ${shaderOutput} ${PROJECT_SOURCE_DIR}/src
-		)
-		list(APPEND compiledShaders ${shaderOutput})
-	endif()
+foreach(dir ${ASSETS_DIRS})
+	file(GLOB_RECURSE srcShaders RELATIVE ${ASSETS_RELATIVE_SRC}/ ${dir}/shaders/**)
+	foreach(shader ${srcShaders})
+		string(REGEX MATCH ".*/[a-zA-Z0-9][^/]*\.(conf|glsl|hlsl|vert|tesc|tese|geom|frag|comp|mesh|task|rgen|rint|rahit|rchit|rmiss|rcall)$" IsShader ${shader})
+		if(IsShader)
+			string(REGEX REPLACE "^v4d/" "" shaderPath ${shader})
+			string(REGEX REPLACE "\.glsl$" "" shaderFileName ${shaderPath})
+			set(shaderInput ${ASSETS_RELATIVE_SRC}/${shader})
+			set(shaderOutput ${RUNTIME_OUTPUT_DIRECTORY}/${shaderFileName}.meta)
+			add_custom_command(
+				MAIN_DEPENDENCY ${shaderInput}
+				OUTPUT ${shaderOutput} POST_BUILD
+				WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+				COMMAND ${CMAKE_BINARY_DIR}/shadercompiler${CMAKE_EXECUTABLE_SUFFIX} ${shaderInput} ${shaderOutput} ${PROJECT_SOURCE_DIR}/src
+			)
+			list(APPEND compiledShaders ${shaderOutput})
+		endif()
+	endforeach()
 endforeach()
 
 add_custom_target(shaders ALL DEPENDS ${compiledShaders})
