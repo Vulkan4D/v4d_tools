f6b751f2f1daef95fc2b8b09964973a4d154bc86
olivier st-laurent <kzaurckichz@gmail.com>
2020-05-09 15:44:40 -0400

    ..

--- initNewGitRepository.sh
+++ initNewGitRepository.sh
@@ -1,8 +1,8 @@
 cd `dirname $0`/..
 git remote remove origin
-current=`git branch --show-current`
+current="`git branch --show-current`"
 git checkout --orphan _newproject
 git add -A
 git commit -m'initial commit'
-git branch -D $current
+git branch -D "$current"
 git branch -m master
